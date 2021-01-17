84ec2facc4009d2849972855966394c995732b8a
maulika27 <68596398+maulika27@users.noreply.github.com>
2021-01-17 15:09:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload(){
   
   playerRunning = loadAnimation("Monkey_01.png","Monkey_02.png", "Monkey_03.png", "Monkey_04.png", "Monkey_05.png", "Monkey_06.png", "Monkey_07.png", "Monkey_08.png","Monkey_09.png", "Monkey_10.png")
   
-  bananaImage = loadImage("Banana.png")
+  bananaImage = loadImage("banana.png")
   
   stoneImage = loadImage("stone.png")
   
